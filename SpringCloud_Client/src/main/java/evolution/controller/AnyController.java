0cafedf370592d50b1a048049a0c7f91e6961e96
chenli <chenli@192.168.43.24>
2017-05-02 16:18:11 +0800

    refactor

--- SpringCloud_Client/src/main/java/evolution/controller/AnyController.java
+++ SpringCloud_Client/src/main/java/evolution/controller/AnyController.java
@@ -10,8 +10,17 @@ import evolution.util.Sys;
 public class AnyController {
 	// This property is provided by spring cloud config server.
 	// This property in fact comes from https://github.com/fslichen/SpringCloud_Configuration.
+	// Corresponds to application.properties from SpringCloud_Github, which is always fetched.
 	@Value("${message}")
 	private String message;
+	
+	// Corresponds to any-configuration.properties from SpringCloud_Github.
+	@Value("${name}")
+	private String name;
+	
+	// Corresponds to any-configuration.properties from SpringCloud_Github.
+	@Value("${gender}")
+	private String gender;
 	
 	// Corresponds to server.port in application.properties in the current project.
 	// The port number of the client and the spring cloud config server should be different.
@@ -23,5 +32,7 @@ public class AnyController {
 		Sys.println("Welcome to the client.");
 		Sys.println("Port = " + port);
 		Sys.println("Message = " + message);
+		Sys.println("Name = " + name);
+		Sys.println("Gender = " + gender);
 	}
 }
